f027a08feb537323f69d0a576299c9bf81b0dc29
Daniel Moya <damr67@gmail.com>
2020-03-20 00:37:28 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { Pane, Text } from 'evergreen-ui';
+import { Pane } from 'evergreen-ui';
 
 function App() {
   return (
-    <Pane
-      height={120}
-      width={240}
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      border="default"
-    >
-      <Text>Pane</Text>
-    </Pane>
+    <Pane />
   );
 }
 
